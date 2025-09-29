3a864e278f7bb1c4cdfd3dd34da487af494b807b
Niamh Cuileann <sweet.niamhy@gmail.com>
2025-09-29 10:11:54 -0400

    Moved kangaru and entt into their own branches, hopefully can get some official support for these

--- src/core/kangaru.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <kangaru/kangaru.hpp>
-
-namespace kgr::detail {
-	default_inject_function_t default_inject_function{};
-	in_place_t in_place{};
-	override_index_t override_index{};
-} // namespace kgr::detail
